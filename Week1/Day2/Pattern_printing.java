ce15ad4998797b5ce2414c63aa2df1da1fb4238c
Prem <vermaprem.pm@gmail.com>
2024-07-16 16:15:24 +0530

    Add Patterns

--- Week1/Day2/Pattern_printing.java
+++ Week1/Day2/Pattern_printing.java
@@ -1,7 +1,91 @@
 package Week1.Day2;
+import java.util.*;
 
 public class Pattern_printing {
+
+    public static void pattern1(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    //$ * * * * * 
+    //$ * * * * * 
+    //$ * * * * * 
+    //$ * * * * * 
+    //$ * * * * *  
+
+
+    public static void pattern2(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    //$ * 
+    //$ * *
+    //$ * * *
+    //$ * * * *
+    //$ * * * * *
+
+
+    public static void pattern3(int n){
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    //$ * * * * * 
+    //$ * * * *
+    //$ * * *
+    //$ * *
+    //$ *
+
+    public static void pattern4(int n){
+        for(int i=1;i<=n;i++){
+            for(int k = 1;k<=n-i;k++){
+                System.out.print("  ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    //$         * 
+    //$       * *
+    //$     * * *
+    //$   * * * *
+    //$ * * * * *
+
+    public static void pattern5(int n){
+        for(int i=n;i>=1;i--){
+            for(int k = 1;k<=n-i;k++){
+                System.out.print("  ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    //$ * * * * * 
+    //$   * * * * 
+    //$     * * * 
+    //$       * * 
+    //$         *
+
     public static void main(String[] args) {
-        System.out.println("Prem");
+        System.out.print("Enter the input = ");
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+        pattern5(n);
+        sc.close();
     }
 }
